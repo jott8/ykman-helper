4d92fffe8fa8ab0fabdead3335bfe2640f1a582e
jott8 <>
2024-06-02 12:05:05 +0200

    format results for listing oath entries

--- app/Ykham.hs
+++ app/Ykham.hs
@@ -4,16 +4,38 @@ import System.Process         ( readProcessWithExitCode )
 import System.Environment     ( getArgs )
 import System.Hclip           ( setClipboard )
 
-import Control.Monad.Except
+import Control.Monad.Except   ( runExceptT, MonadError(throwError), ExceptT )
 import Control.Monad.IO.Class ( liftIO )
 import GHC.IO.Exception       ( ExitCode (ExitSuccess) )
 
 import Options.Applicative
+    ( (<**>),
+      argument,
+      command,
+      fullDesc,
+      header,
+      help,
+      info,
+      long,
+      metavar,
+      progDesc,
+      short,
+      str,
+      subparser,
+      switch,
+      execParser,
+      helper,
+      ParserInfo )
 import qualified Options.Applicative as Opt
 
-import Data.Text (Text)
+import Data.Text ( Text )
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
+
+import Data.List     ( sort )
+import Data.Function ( on )
+
+import Fmt ( (+|), fmt, (|+), blockMapF', nameF, Buildable(build) )
 
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
@@ -32,10 +54,16 @@ type Code     = Text
 type Issuer   = Text
 type Username = Text
 
+type Entries  = Map Int Entry
+
 data Entry 
     = MinE Username
     | FullE Issuer Username
-    deriving (Eq, Show)
+    deriving ( Eq, Show )
+
+instance Ord Entry where
+    (MinE u)     `compare` (FullE i _)  = (compare `on` T.length) u i
+    (FullE i1 _) `compare` (FullE i2 _) = (compare `on` T.length) i1 i2
 
 createEntry :: Text -> Entry
 createEntry input = 
@@ -49,23 +77,28 @@ createEntry input =
 
 ykman :: [Argument] -> Stdin -> YkmanM Text
 ykman args stdin = do
-    result@(_,s,e) <- liftIO $ readProcessWithExitCode "ykman" (map T.unpack args) (T.unpack stdin)
+    result@(_,s,e) <- liftIO $ readProcessWithExitCode "ykman" args' stdin'
     if isSuccess result 
         then pure $ T.pack s
         else throwError $ T.pack e
 
     where
         isSuccess (ExitSuccess,s,_) = not $ null s
-        isSuccess (_,_,e)           = False
+        isSuccess _                 = False
+
+        args' = map T.unpack args
+        stdin' = T.unpack stdin
 
 readOath :: Query -> YkmanM Code
 readOath q = head . T.lines <$> ykman ["oath", "accounts", "code", "-s", q] ""  
           -- ^^^^ safe usage of ´head´ because of ´null´ check in ´isSuccess´
-      
-listOath :: YkmanM (Map Int Entry)
+
+listOath :: YkmanM Entries
 listOath = M.fromList <$> indexedEntries
     where
-        indexedEntries = index . map createEntry . T.lines <$> ykman ["oath", "accounts", "list"] ""
+        indexedEntries = index <$> entries
+        entries        = map createEntry <$> textEntries
+        textEntries    = T.lines <$> ykman ["oath", "accounts", "list"] ""
 
 --------------------------------------------------------------------
 -- HANDLING RESULTS / UTILS
@@ -82,6 +115,20 @@ clipPrintCode c = setClipboard (T.unpack c) >> printCode c
 
 handleResult :: (a -> IO ()) -> Either Text a -> IO ()
 handleResult = either print
+
+printList :: Entries -> IO ()
+printList entries = 
+    if M.size entries <= 0
+        then TIO.putStrLn "No entries yet..."
+        else TIO.putStrLn . fmt . nameF "Your oath entries:" . blockMapF' buildKey buildVal $ entries
+    where
+        buildKey             = build
+        buildVal (MinE u)    = build u 
+        buildVal (FullE i u) = ""+|T.justifyLeft longest ' ' i|+" - "+|u|+""
+
+        longest              = getLen $ maximum entries
+        getLen (MinE u)      = T.length u
+        getLen (FullE i _)   = T.length i
 
 index :: [a] -> [(Int,a)]
 index = zip [1..]
@@ -130,7 +177,7 @@ main = run =<< execParser opts
 run :: Options -> IO ()
 run (Options (Read q True))  = runExceptT (readOath q) >>= handleResult clipPrintCode
 run (Options (Read q False)) = runExceptT (readOath q) >>= handleResult printCode 
-run (Options (List _))       = runExceptT listOath     >>= handleResult print
+run (Options (List _))       = runExceptT listOath     >>= handleResult printList
 
 -- TODO: result formatting
 -- TODO: updating entries
